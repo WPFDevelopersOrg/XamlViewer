d1621ec53326fec291523b47eb598479081b3922
huangjia <654664168@qq.com>
2019-11-19 09:35:24 +0800

    add base layout

--- XamlViewer/App.xaml.cs
+++ XamlViewer/App.xaml.cs
@@ -5,13 +5,23 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Prism.DryIoc;
+using Prism.Ioc;
 
 namespace XamlViewer
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(Prism.Ioc.IContainerRegistry containerRegistry)
+        {
+        }
     }
 }
